85276ad4897b0cf598cd13ea3e9bcabd8ecd9b57
Zackery Leman <zleman1593@gmail.com>
2015-08-12 14:06:17 -0700

    improved swal

--- client/js/client.js
+++ client/js/client.js
@@ -142,240 +142,247 @@ Template.landing.events({
 
 Template.codeBox.onRendered(function() {
 
-            var room = Session.get("currentCodeId");
-            Streamy.on(room, function(d, s) {
-                console.log(d.data);
-                if (d.data === "startVideo") {
-                    swal({
-                        title: "Incoming Video Call",
-                        text: "Your partner would like to video chat with you.",
-                        type: "input",
-                        showCancelButton: true,
-                        closeOnConfirm: false,
-                        animation: "slide-from-top",
-                        inputValue: "filename." + $('option:selected', $("select")).attr("ext"),
-                        confirmButtonColor: "rgb(24, 188, 156)",
-                        confirmButtonText: "Save"
-
-                    });
+    var room = Session.get("currentCodeId");
+    Streamy.on(room, function(d, s) {
+        console.log(d.data);
+        if (d.data === "startVideo") {
+            swal({
+                title: "Incoming Video Call",
+                text: "Your partner would like to video chat with you.",
+                type: "warning",
+                showCancelButton: true,
+                animation: "slide-from-top",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Join Call!",
+                cancelButtonText: "Decline call",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            }, function(isConfirm) {
+                if (isConfirm) {
+                 swal("Accepted", "You have accepted the call :)", "success");
+                } else {
+                	 swal("Declined!", "Your have declined the call.", "error");
+                   
                 }
             });
+        }
+    });
 
 
-                $('select').selectric().on('selectric-change', function(element) {
-                    var mode = $(this).val();
-                    ace.edit("codeBox").getSession().setMode('ace/mode/' + mode);
+    $('select').selectric().on('selectric-change', function(element) {
+        var mode = $(this).val();
+        ace.edit("codeBox").getSession().setMode('ace/mode/' + mode);
 
-                    //update stored groupcode language
-                    Meteor.call("updateGroupCodeLanguage", Session.get("currentCodeId"), $("select").val());
-                });
+        //update stored groupcode language
+        Meteor.call("updateGroupCodeLanguage", Session.get("currentCodeId"), $("select").val());
+    });
+});
+
+Template.codeBox.helpers({
+    // helper functions go here
+    'code': function() {
+        var codeBase = Code.findOne({
+            _id: Session.get("currentCodeId")
+        });
+        return codeBase ? codeBase.code : "";
+    },
+    'docid': function() {
+        return Session.get("currentCodeId");
+    },
+    'config': function() {
+        return function(editor) {
+            editor.setTheme('ace/theme/twilight');
+            editor.getSession().setMode('ace/mode/' + $("select").val());
+            editor.setShowPrintMargin(false);
+            editor.getSession().setUseWrapMode(true);
+
+            var codeBase = Code.findOne({
+                _id: Session.get("currentCodeId")
             });
 
-            Template.codeBox.helpers({
-                // helper functions go here
-                'code': function() {
-                    var codeBase = Code.findOne({
-                        _id: Session.get("currentCodeId")
-                    });
-                    return codeBase ? codeBase.code : "";
-                },
-                'docid': function() {
-                    return Session.get("currentCodeId");
-                },
-                'config': function() {
-                    return function(editor) {
-                        editor.setTheme('ace/theme/twilight');
-                        editor.getSession().setMode('ace/mode/' + $("select").val());
-                        editor.setShowPrintMargin(false);
-                        editor.getSession().setUseWrapMode(true);
+            if (codeBase) {
+                $("select").val(codeBase.language).selectric("refresh");
+                ace.edit("codeBox").getSession().setMode('ace/mode/' + codeBase.language);
+            }
+        }
+    },
+    'fileUploaded': function() {
+        return function(editor) {
+            if (Session.get("fileUploaded")) {
+                editor.setValue(Session.get("fileUploaded"));
 
-                        var codeBase = Code.findOne({
-                            _id: Session.get("currentCodeId")
-                        });
-
-                        if (codeBase) {
-                            $("select").val(codeBase.language).selectric("refresh");
-                            ace.edit("codeBox").getSession().setMode('ace/mode/' + codeBase.language);
-                        }
+                if (ext = Session.get("fileExtension")) {
+                    if (option = $("option[ext='" + ext + "']").attr("value")) {
+                        $("select").val(option).selectric("refresh");
+                        ace.edit("codeBox").getSession().setMode('ace/mode/' + option);
+                        Meteor.call("updateGroupCodeLanguage", Session.get("currentCodeId"), $("select").val());
                     }
-                },
-                'fileUploaded': function() {
-                    return function(editor) {
-                        if (Session.get("fileUploaded")) {
-                            editor.setValue(Session.get("fileUploaded"));
-
-                            if (ext = Session.get("fileExtension")) {
-                                if (option = $("option[ext='" + ext + "']").attr("value")) {
-                                    $("select").val(option).selectric("refresh");
-                                    ace.edit("codeBox").getSession().setMode('ace/mode/' + option);
-                                    Meteor.call("updateGroupCodeLanguage", Session.get("currentCodeId"), $("select").val());
-                                }
-                                Session.set("fileExtension", null);
-                            }
-
-                            Session.set("fileUploaded", null);
-                        }
-                    }
+                    Session.set("fileExtension", null);
                 }
-            });
 
-            Template.videoChat.events({
-                'click #videoControl': function() {
-                    var newVal = !Session.get("videoMuted");
-                    Session.set("videoMuted", newVal);
-                    if (newVal) {
-                        $("#videoControl").removeClass("btn-success").addClass("btn-danger");
-                        $("#videoControl>span").removeClass("glyphicon-eye-open").addClass("glyphicon-eye-close");
-                        webrtc.pauseVideo();
-                    } else {
-                        $("#videoControl").removeClass("btn-danger").addClass("btn-success");
-                        $("#videoControl>span").removeClass("glyphicon-eye-close").addClass("glyphicon-eye-open");
-                        webrtc.resumeVideo();
-                    }
-                },
-                'click #audioControl': function() {
-                    var newVal = !Session.get("audioMuted");
-                    Session.set("audioMuted", newVal);
-                    if (newVal) {
-                        $("#audioControl").removeClass("btn-success").addClass("btn-danger");
-                        $("#audioControl>span").removeClass("glyphicon-volume-up").addClass("glyphicon-volume-off");
-                        webrtc.mute();
-                    } else {
-                        $("#audioControl").removeClass("btn-danger").addClass("btn-success");
-                        $("#audioControl>span").removeClass("glyphicon-volume-off").addClass("glyphicon-volume-up");
-                        webrtc.unmute();
-
-                    }
-                },
-                'click #sizeControl': function() {
-                    var newVal = !Session.get("fullscreen");
-                    Session.set("fullscreen", newVal);
-                    if (newVal) {
-                        $(".videoChatWrapper").addClass("fullscreen");
-                        $("#sizeControl").removeClass("btn-success").addClass("btn-danger");
-                        $("#sizeControl>span").removeClass("glyphicon-resize-full").addClass("glyphicon-resize-small");
-                    } else {
-                        $(".videoChatWrapper").removeClass("fullscreen");
-                        $("#sizeControl").removeClass("btn-danger").addClass("btn-success");
-                        $("#sizeControl>span").removeClass("glyphicon-resize-small").addClass("glyphicon-resize-full");
-                    }
-                },
-            });
-
-            function nameAnimation() {
-                var tempName = "**********".split("");
-                var realName = "GroupCodes".split("");
-                Session.set("name", tempName.join(""));
-
-                var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                var lower = "abcdefghijklmnopqrstuvwxyz";
-
-                var i = 0;
-                var j = 0;
-                var animation = setInterval(function() {
-                    if (tempName[i] != realName[i]) {
-                        if (capital.indexOf(realName[i]) >= 0) {
-                            var newLetter = capital[j % capital.length];
-                        } else {
-                            var newLetter = lower[j % lower.length];
-                        }
-                        tempName[i] = newLetter;
-                        j++;
-                    } else {
-                        i++;
-                        j = 0;
-                    }
-
-                    if (i == realName.length && tempName == realName) {
-                        clearInterval(animation);
-                    }
-
-                    Session.set("name", tempName.join(""));
-                }, 10);
+                Session.set("fileUploaded", null);
             }
+        }
+    }
+});
 
-            //trigger download on ctrl+s
-            function triggerDownload(event) {
-                if (Router.current().route.getName() == "groupcode.codebox") {
-                    if (event.keyCode == 83 && (navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey)) {
-                        event.preventDefault();
-                        download(ace.edit("codeBox").getValue());
-                    }
-                }
+Template.videoChat.events({
+    'click #videoControl': function() {
+        var newVal = !Session.get("videoMuted");
+        Session.set("videoMuted", newVal);
+        if (newVal) {
+            $("#videoControl").removeClass("btn-success").addClass("btn-danger");
+            $("#videoControl>span").removeClass("glyphicon-eye-open").addClass("glyphicon-eye-close");
+            webrtc.pauseVideo();
+        } else {
+            $("#videoControl").removeClass("btn-danger").addClass("btn-success");
+            $("#videoControl>span").removeClass("glyphicon-eye-close").addClass("glyphicon-eye-open");
+            webrtc.resumeVideo();
+        }
+    },
+    'click #audioControl': function() {
+        var newVal = !Session.get("audioMuted");
+        Session.set("audioMuted", newVal);
+        if (newVal) {
+            $("#audioControl").removeClass("btn-success").addClass("btn-danger");
+            $("#audioControl>span").removeClass("glyphicon-volume-up").addClass("glyphicon-volume-off");
+            webrtc.mute();
+        } else {
+            $("#audioControl").removeClass("btn-danger").addClass("btn-success");
+            $("#audioControl>span").removeClass("glyphicon-volume-off").addClass("glyphicon-volume-up");
+            webrtc.unmute();
+
+        }
+    },
+    'click #sizeControl': function() {
+        var newVal = !Session.get("fullscreen");
+        Session.set("fullscreen", newVal);
+        if (newVal) {
+            $(".videoChatWrapper").addClass("fullscreen");
+            $("#sizeControl").removeClass("btn-success").addClass("btn-danger");
+            $("#sizeControl>span").removeClass("glyphicon-resize-full").addClass("glyphicon-resize-small");
+        } else {
+            $(".videoChatWrapper").removeClass("fullscreen");
+            $("#sizeControl").removeClass("btn-danger").addClass("btn-success");
+            $("#sizeControl>span").removeClass("glyphicon-resize-small").addClass("glyphicon-resize-full");
+        }
+    },
+});
+
+function nameAnimation() {
+    var tempName = "**********".split("");
+    var realName = "GroupCodes".split("");
+    Session.set("name", tempName.join(""));
+
+    var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+
+    var i = 0;
+    var j = 0;
+    var animation = setInterval(function() {
+        if (tempName[i] != realName[i]) {
+            if (capital.indexOf(realName[i]) >= 0) {
+                var newLetter = capital[j % capital.length];
+            } else {
+                var newLetter = lower[j % lower.length];
             }
+            tempName[i] = newLetter;
+            j++;
+        } else {
+            i++;
+            j = 0;
+        }
 
-            function download(text) {
-                swal({
-                    title: "Save File",
-                    text: "Enter a filename:",
-                    type: "input",
-                    showCancelButton: true,
-                    closeOnConfirm: false,
-                    animation: "slide-from-top",
-                    inputValue: "filename." + $('option:selected', $("select")).attr("ext"),
-                    confirmButtonColor: "rgb(24, 188, 156)",
-                    confirmButtonText: "Save",
-                }, function(filename) {
-                    if (filename === false)
-                        return false;
-                    else if (filename === "") {
-                        swal.showInputError("You need to write something!");
-                        return false
-                    } else {
-                        var element = document.createElement('a');
-                        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-                        element.setAttribute('download', filename);
+        if (i == realName.length && tempName == realName) {
+            clearInterval(animation);
+        }
 
-                        element.style.display = 'none';
-                        document.body.appendChild(element);
+        Session.set("name", tempName.join(""));
+    }, 10);
+}
 
-                        element.click();
+//trigger download on ctrl+s
+function triggerDownload(event) {
+    if (Router.current().route.getName() == "groupcode.codebox") {
+        if (event.keyCode == 83 && (navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey)) {
+            event.preventDefault();
+            download(ace.edit("codeBox").getValue());
+        }
+    }
+}
 
-                        document.body.removeChild(element);
-                        swal.close();
-                    }
-                });
+function download(text) {
+    swal({
+        title: "Save File",
+        text: "Enter a filename:",
+        type: "input",
+        showCancelButton: true,
+        closeOnConfirm: false,
+        animation: "slide-from-top",
+        inputValue: "filename." + $('option:selected', $("select")).attr("ext"),
+        confirmButtonColor: "rgb(24, 188, 156)",
+        confirmButtonText: "Save",
+    }, function(filename) {
+        if (filename === false)
+            return false;
+        else if (filename === "") {
+            swal.showInputError("You need to write something!");
+            return false
+        } else {
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            element.setAttribute('download', filename);
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+            swal.close();
+        }
+    });
+}
+
+function createGroupCode() {
+    //create groupcode on the server
+    Meteor.call("createGroupCode", "javascript", function(err, codeId) {
+        if (!err) {
+            Session.set("currentCodeId", codeId);
+            Router.go("/" + codeId);
+        }
+    });
+}
+
+/* FILE API */
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt = evt.originalEvent;
+
+    //gets files from either upload button or drag & drop
+    var files = evt.dataTransfer ? evt.dataTransfer.files : evt.target.files; // FileList object
+
+    //we only support one file at a time
+    var file = files[0];
+
+    //if file exists, read file & store it
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = function(evt) {
+            Session.set("fileUploaded", evt.target.result);
+
+            //get file extension
+            var ext = file.name.match(/\.([0-9a-z]+)(?:[\?#]|$)/);
+            if (ext.length > 0) {
+                ext = ext[0].substring(1);
+                Session.set("fileExtension", ext);
             }
-
-            function createGroupCode() {
-                //create groupcode on the server
-                Meteor.call("createGroupCode", "javascript", function(err, codeId) {
-                    if (!err) {
-                        Session.set("currentCodeId", codeId);
-                        Router.go("/" + codeId);
-                    }
-                });
-            }
-
-            /* FILE API */
-
-            function handleFileSelect(evt) {
-                evt.stopPropagation();
-                evt.preventDefault();
-                evt = evt.originalEvent;
-
-                //gets files from either upload button or drag & drop
-                var files = evt.dataTransfer ? evt.dataTransfer.files : evt.target.files; // FileList object
-
-                //we only support one file at a time
-                var file = files[0];
-
-                //if file exists, read file & store it
-                if (file) {
-                    var reader = new FileReader();
-                    reader.readAsText(file, "UTF-8");
-                    reader.onload = function(evt) {
-                        Session.set("fileUploaded", evt.target.result);
-
-                        //get file extension
-                        var ext = file.name.match(/\.([0-9a-z]+)(?:[\?#]|$)/);
-                        if (ext.length > 0) {
-                            ext = ext[0].substring(1);
-                            Session.set("fileExtension", ext);
-                        }
-                    }
-                }
-            }
+        }
+    }
+}
 
 
-            /* END FILE API */
+/* END FILE API */
