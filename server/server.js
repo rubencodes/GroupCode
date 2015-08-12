2c042f56e024adb38a56830857307426f267a1e4
Zackery Leman <zleman1593@gmail.com>
2015-08-12 14:37:46 -0700

    Security update to prevent DDOS

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 // on the server
 Meteor.publish('codebase', function(id) {
+	check(id,String);
   return Code.find({ _id : id });
 });
 
 Meteor.methods({
 	"createGroupCode" : function(language) {
+		check(language,String);
 		return Code.insert({ language: language });
 	},
 	"updateGroupCodeLanguage" : function(id, language) {
+		check(language,String);
+		check(id,String);
 		Code.update({ _id: id }, { $set : { language: language } });
 	},
 });
+
